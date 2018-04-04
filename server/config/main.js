92686fe4234e630483835da00d941b3e14aa6e23
jbryyan <jbryyan@gmail.com>
2018-04-03 17:15:30 -0700

    Server files started

---
+++ server/config/main.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  'secret': 'webdevjs',
+  'database': 'mongodb://localhost:27017/night-app'
+};
---
+++ server/controllers/register.js
@@ -0,0 +1,34 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const config = require('../config/main');
+
+module.exports = function(req, res){
+  console.log(req.body.username);
+  console.log(req.body.password);
+  if(!req.body.username || !req.body.password) {
+    res.json({ success: false, message: 'Please enter a username and password to register.' });
+  } else {
+    console.log("In register let new user.")
+    let newUser = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    console.log(newUser);
+    // Attempt to save new user
+    newUser.save((err) => {
+        console.log(err);
+        if (err && err.code === 11000) return res.status(401).json({ success: false, message: 'Username must be unique' });
+        else if (err) return res.status(401).json({ success: false, message: err.errors.username.message });
+        else{ 
+
+          jwt.sign({ username: req.body.username }, config.secret, { expiresIn: '1h' },
+          function(err,token){
+              if (err) throw err;
+              res.setHeader('Authorization', 'Bearer ' + token);
+              res.status(200).json({ success: true, username: req.body.username});
+          });
+
+        }
+    });
+  }
+};
