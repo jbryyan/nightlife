d0b369db03b035a9fb6691a291dca63ae041d982
jbryyan <jbryyan@gmail.com>
2018-04-18 02:04:50 -0700

    Cleanup code is next. Project server files set up on glitch.com

--- server/config/main.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  'secret': 'webdevjs',
-  'database': 'mongodb://localhost:27017/night-app',
-  'yelpsearch': 'https://api.yelp.com/v3/businesses/search',
-  'yelpkey': 'Bearer rOEU7KrjOd1B1uVvFL6YpuTOKieM7efaqnuVO15MYFgL85qjuv9oTb9q3yiuqfy2cs6pcUGeImO_FsvVqkOfCkr2OqF32VdhcMlCp8-rDuZ-XHQd-obKxxq1gx_EWnYx'
-};
